e2c288efa6bc25950269f2bad9d564d59c231653
MPSAJID <122434439+MPSAJID@users.noreply.github.com>
2025-02-26 13:38:11 +0530

    todo backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,52 @@
 const express = require('express');
+require('dotenv').config();
+const { createTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 
 app.use(express.json());
 
-app.post('/todo',(req,res)=>{
+app.post('/todo',async (req,res)=>{
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong input",
+        })
+        return;
+    }
+    //if success store in db
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed : false
+    })
 
+    res.json({
+        msg : "todo created"
+    })
 });
 
-app.get('/todos',(req,res)=>{
-
+app.get('/todos',async (req,res)=>{
+    const todos = await todo.find();
+    res.json({
+        todos
+    })
 });
 
-app.put('/completed',(req,res)=>{
-
+app.put('/completed',async (req,res)=>{
+    const updatePayload = req.body;
+    const parsedPayload = createTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong input",
+        })
+        return;
+    }
+    await todo.update({_id:req.body},{completed:true});
+    res.json({
+        msg: "todo marked as completed"
+    })
 })
 
 app.listen(3000,()=>{
